0f4b1f9596d908e8ac694beccbad531ee63a274f
Tayler Porter <taylerporter@gmail.com>
2022-05-17 21:49:14 -0500

    Added temporary projects.php

---
+++ projects.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php $PAGE_TITLE = "Projects"; require('head.php'); ?>
+    </head>
+    <body>
+        <?php require('nav.php'); ?>
+        <div class="container">
+            <h2>My Projects</h2>
+            <p>I've worked on quite a few projects in my time, like this very website.  This page is just temporary, so for now here's some lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt magna finibus ex ultricies interdum. Morbi a orci sit amet neque pellentesque maximus in vitae erat. Morbi pellentesque nunc sed lectus lobortis, vel tristique magna viverra. Maecenas sapien tortor, rhoncus sed sem et, blandit condimentum quam. Phasellus sollicitudin arcu non augue pellentesque, id dictum tellus maximus. Nulla volutpat mauris lectus, ut tincidunt lacus malesuada a. Phasellus nec quam ex. Phasellus et ultricies nisi. Quisque sed nibh lobortis, tempus nulla et, vehicula eros. Nullam tincidunt metus a laoreet congue. Nam suscipit elit lectus, eget auctor dolor dictum rutrum. Aenean at leo leo. Fusce efficitur, eros sit amet lacinia semper, justo dui tristique lorem, quis auctor velit eros quis dolor. Praesent et gravida erat. Fusce ac turpis ante. Duis dapibus lacinia dui, nec scelerisque magna aliquet sit amet.</p>
+        </div>
+    </body>
+</html>
