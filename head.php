339bb2ec4409c0e9392e4486229c57999b2ed37a
Tayler Porter <taylerporter@gmail.com>
2022-05-17 21:37:42 -0500

    Added hyperlinks for information in about me. Added certificates.

--- head.php
+++ head.php
@@ -4,8 +4,19 @@
 <script src="bootstrap-5.0.2\js\bootstrap.bundle.js"></script>
 
 <style>
-    h2{
+    h2, h3{
         text-align: center;
+        padding: 25px;
+    }
+    .ill{  /* in line link */
+        color: rgba(0, 0, 0, 0.55);
+
+    }
+    .ill:hover, .ill:focus{
+        color: rgba(0, 0, 0, 0.7);
+    }
+    .comptia{
+        width: 30%;
         padding: 25px;
     }
 </style>
--- index.php
+++ index.php
@@ -7,7 +7,13 @@
         <?php require('nav.php'); ?>
         <div class="container">
             <h2>About Me</h2>
-            <p>My name is Tayler "TJ" Porter, I am currently a Senior Airman in the United States Air Force working as a Cyber Defense Operations Team Lead. I am a certified CompTIA Network Vulnerability Assessment Professional, meaning I have obtained both my CompTIA Security+ (SY0-501) and CompTIA PenTest+ (PT0-002).</p>
+            <p>My name is Tayler "TJ" Porter, I am currently a Senior Airman in the United States Air Force working as a Cyber Defense Operations Team Lead. I am a certified <a class="ill" href="https://www.credly.com/badges/8143abc2-d008-4cc8-b4fc-8be9f5729e17">CompTIA Network Vulnerability Assessment Professional</a> (<a class="ill" href="https://www.comptia.org/certifications/which-certification/stackable-certifications">CNVP</a>), meaning I have obtained both my <a class="ill" href="https://www.credly.com/badges/ed029896-f1be-41fe-b633-76328b6fb8a7">CompTIA Security+</a> (<a class="ill" href="https://www.comptia.org/certifications/security">SY0-501</a>) and <a class="ill" href="https://www.credly.com/badges/e274e164-c166-4a71-a05d-687ccd67fd4c">CompTIA PenTest+</a> (<a class="ill" href="https://www.comptia.org/certifications/pentest">PT0-002</a>).</p>
+            <h3>Certifications</h3>
+            <center>
+                <a href="/res/CNVP.pdf"><img class="comptia" src="/res/CNVP.jpg"></a>
+                <a href="/res/PenTest.pdf"><img class="comptia" src="/res/PenTest.png"></a>
+                <a href="/res/Security.pdf"><img class="comptia" src="/res/Security.jpg"></a>
+            </center>
         </div>
     </body>
 </html>
